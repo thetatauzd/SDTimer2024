ec970185f8e9373d72d78bb0da3415bfbe57b095
Matthew Hughes <matthewhughes1300@gmail.com>
2024-02-01 11:41:49 -0500

    Created the speed dating timer

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SDTimer2024
+## Requirements
+```pip install -r requirements.txt```
+## How to Use
+1) Install the required packages
+2) Change the station and rotation times(in seconds)
+3) Click on the timer link and make that match
+4) Run the program
---
+++ SpeedTimer.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+browser = webdriver.Chrome()
+browser.get('https://chronograph.io/bDZ6FdfU')
+
+time.sleep(2)
+
+div1 = browser.find_element(value='19208847197')
+# b1 = div1.find_element_by_class_name('action')
+b1 = div1.find_element(By.CLASS_NAME, 'action')
+
+div2 = browser.find_element(value='7135425933400')
+# b2 = div2.find_element_by_class_name('action')
+b2 = div2.find_element(By.CLASS_NAME, 'action')
+
+time.sleep(.5)
+
+stationTime = 90
+rotationTime = 30
+
+while True:
+    b1.click()
+    time.sleep(stationTime + 1)
+    b1.click()
+    time.sleep(.5)
+    b2.click()
+    time.sleep(rotationTime + 1)
+    b2.click()
+    time.sleep(.5)
