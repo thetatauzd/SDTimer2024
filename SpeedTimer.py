afbb5b2a818d73c5fe6e6903da2e8f7361f235e9
Matthew Hughes <matthewhughes1300@gmail.com>
2025-02-03 21:01:38 -0500

    Updated browser elements for new IDs

--- SpeedTimer.py
+++ SpeedTimer.py
@@ -8,18 +8,18 @@ browser.get('https://chronograph.io/bDZ6FdfU')
 
 time.sleep(2)
 
-div1 = browser.find_element(value='19208847197')
+div1 = browser.find_element(value='7135425933400')
 # b1 = div1.find_element_by_class_name('action')
 b1 = div1.find_element(By.CLASS_NAME, 'action')
 
-div2 = browser.find_element(value='7135425933400')
+div2 = browser.find_element(value='7578220432574')
 # b2 = div2.find_element_by_class_name('action')
 b2 = div2.find_element(By.CLASS_NAME, 'action')
 
 time.sleep(.5)
 
-stationTime = 90
-rotationTime = 30
+stationTime = 121
+rotationTime = 31
 
 while True:
     b1.click()
